403788e98e2d572e648de042c05250d6093ffb1d
matthewcv <accounts@matthewcv.com>
2015-01-28 22:52:19 -0600

    building out UI
    show method, route, starting on route params

--- WebApiTestClient.Web/Scripts/WebApiTestClient.js
+++ WebApiTestClient.Web/Scripts/WebApiTestClient.js
@@ -1,7 +1,8 @@
 ﻿(function() {
 
+    var apiDescription = null;
+
    getJson("/WebApiTestClient.axd", function(data) {
-       console.dir(data);
        findThisApi(data);
    });
 
@@ -11,24 +12,24 @@
         var apiId = window.location.pathname.split('/').pop();
 
         var found = apis.ApiNames.filter(function(n) {
-            return n == apiId
+            return n == apiId;
         });
 
         if (found.length) {
             console.log(found[0]);
 
-            getJson("/WebApiTestClient.axd?ApiName=" + found[0], generateUi);
+            getJson("/WebApiTestClient.axd?ApiName=" + found[0], getReadyToUseTestClient);
         }
 
     }
 
-    function generateUi(data) {
-
-        addStyle();
-        makeButton();
-
-
+    function getReadyToUseTestClient(data) {
+        apiDescription = data;
         console.dir(data);
+        addStyle();
+        makeActivatorButton();
+
+
     }
 
 
@@ -61,19 +62,47 @@ function getJson(url, callback) {
         document.head.appendChild(link);
     }
 
-    function makeButton() {
+    function makeActivatorButton() {
         var a = document.createElement("A");
         a.className = "watc-activator";
-        a.appendChild(document.createTextNode("Test this API"));
+        dom.text(a, "Test this API");
         a.setAttribute("href", "#test");
         a.addEventListener("click", function(ev) {
             ev.preventDefault();
-
+            getTemplate(showUi);
+            a.parentNode.removeChild(a);
             console.log("show the ui");
 
         });
 
         document.body.appendChild(a);
+    }
+
+    function showUi() {
+        var panel = document.createElement('div');
+        panel.setAttribute('id', 'watc-panel');
+        panel.innerHTML = document.getElementById('tpl-watc-container').innerHTML;
+        document.body.appendChild(panel);
+
+        dom.text('watc-method', apiDescription.Method);
+        dom.text('watc-route', apiDescription.Route);
+        addRouteParams();
+    }
+
+    function addRouteParams() {
+        apiDescription.RouteParameters.forEach(function(p) {
+            addLabeledInput('$rp.',p, dom.gid('watc-routeparams'));
+        });
+    }
+    function addLabeledInput(idprefix, param, container) {
+        var panel = document.createElement('div');
+        var tpl = dom.gid('tpl-watc-labeled-input').innerHTML;
+        tpl = tpl.replace('{{label-id}}', idprefix + param.Name + '-labelcontainer')
+          .replace('{{input-id}}', idprefix + param.Name + '-inputcontainer');
+        panel.innerHTML = tpl;
+        container.appendChild(panel);
+        dom.text(idprefix + param.Name + '-labelcontainer', param.Name);
+
     }
 
     function getTemplate(callback) {
@@ -99,6 +128,19 @@ function getJson(url, callback) {
         xhr.open("GET", "/Content/WebApiTestClient/views.html");
         xhr.send();
     }
+
+
+    var dom = {
+        gid: function(id) {
+            return document.getElementById(id);
+        },
+
+
+        text: function (el, text) {
+            el = typeof(el) == "string" ? this.gid(el) : el;
+            el.appendChild(document.createTextNode(text));
+        }
+}
 
 
 })();
