14bd953c8c834d2e19640d8e2c613186a1b7b575
matthewcv <accounts@matthewcv.com>
2015-01-30 16:44:00 -0600

    simple dictionary UI

--- WebApiTestClient.Web/Scripts/WebApiTestClient.js
+++ WebApiTestClient.Web/Scripts/WebApiTestClient.js
@@ -4,20 +4,24 @@
 
     //the name of the api is the last segment of the url. 
     var apiId = window.location.pathname.split('/').pop();
+
+    //go get the meta data for this API
     getJson("/WebApiTestClient.axd?ApiName=" + apiId, getReadyToUseTestClient);
 
 
+    //callback from getting meta data.
     function getReadyToUseTestClient(data) {
         apiDescription = data;
         setUpApiDescForUi();
         console.dir(data);
         dom.el("link", {"href": "/Content/WebApiTestClient/styles.css","rel":"stylesheet","type": "text/css"}, document.head);
-            dom.el("script", { "src": "//cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.min.js", "type": "text/javascript" }, document.head);
+        dom.el("script", { "src": "//cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.min.js", "type": "text/javascript" }, document.head);
         makeActivatorButton();
 
 
     }
 
+    //take the metadata and extend the objects a bit to help out with the handlebars templates
     function setUpApiDescForUi() {
         apiDescription.QueryParameters.forEach(function(p) {
             p.id = "$qp." + p.Name;
@@ -35,7 +39,7 @@
         }
     }
 
-
+    //put the button on the screen that the user will click to activate the test client.
     function makeActivatorButton() {
         var a = dom.el("A", {'class':"watc-activator","href": "#test"}, document.body);
 
@@ -51,6 +55,8 @@
 
     }
 
+
+    //callback from the ajax call that gets the template HTML
     function showUi() {
         configHandlebars();
 
@@ -64,7 +70,7 @@
         dom.gid('watc-panel').addEventListener("click", clickHandler);
     }
 
-
+    //invoke the template that builds out the ui for the properties of a complex object then insert the resultant HTML into the dom at the appropriate palce
     function addProperties(id, typeName) {
         if (typeName) {
             var type = apiDescription.ComplexTypes.filter(function(t) {
@@ -87,7 +93,7 @@
         }
     }
 
-
+    //handles any click event from within the panel that contains the test client
     function clickHandler(ev) {
         var cmd = ev.target.getAttribute("data-cmd");
         if (cmd && commands[cmd]) {
@@ -96,8 +102,10 @@
         }
     }
 
+    //set up handlebars.  
     function configHandlebars() {
 
+        //Get all the templates, compile each one, cache the compiled template and register it as a partial
         [].forEach.call(document.querySelectorAll("script[type='text/html']"), function(t) {
 
             var name = t.getAttribute("id").replace("tpl-watc-", "");
@@ -111,8 +119,9 @@
 
     var templates = {};
 
-
+    //commands that respond to various buttons that could be clicked by the user.  functions correspond to data-cmd attribute values in links and buttons in the templates.  
     var commands = {
+
         inputListRemove:function(el) {
             var inputs = el.parentNode.querySelectorAll("input");
             if (inputs.length == 1) {
@@ -124,6 +133,7 @@
 
             el.parentNode.removeChild(last);
         },
+
         inputListAdd:function(el) {
             var inputs = el.parentNode.querySelectorAll("input");
             var insertPoint = inputs[inputs.length - 1].nextSibling;
@@ -160,6 +170,7 @@
             addProperties(newId, typeName);
             //these are the existing objects.
         },
+
         complexListRemove:function(el) {
             var divs = [].filter.call(el.parentNode.childNodes, function (n) {
                 return n.nodeName.toLowerCase() == 'div';
@@ -176,6 +187,46 @@
             }
 
         },
+
+        dictionaryRemove:function(el) {
+            var theUl = dom.prevSib(el, 'ul');
+            var last = theUl.lastChild;
+            if (last.nodeName.toLowerCase() != 'li') {
+                last = dom.prevSib(last, 'li');
+            }
+
+            var prev = dom.prevSib(last, 'li');
+
+            console.dir(prev);
+            if (prev && prev.lastChild.nodeName.toLowerCase() == "#text") {
+                prev.removeChild(prev.lastChild);
+            }
+
+            theUl.removeChild(last);
+        },
+
+        dictionaryAdd: function (el) {
+            var id = el.getAttribute("data-id");
+            var typeName = el.getAttribute("data-type-name");
+
+            var type = apiDescription.ComplexTypes.filter(function (t) {
+                return t.Name == typeName;
+            });
+
+
+            var context = { id: id, IsSimple: type.length==0, ValueTypeName:typeName }
+            var html = templates['dictionary-type-item'](context).trim();
+
+            var theUl = dom.prevSib(el, 'ul');
+
+            dom.html(html, theUl);
+
+            var prevLi = dom.prevSib(theUl.lastChild, "li");
+            if (prevLi) {
+                prevLi.appendChild(document.createTextNode(","));
+            }
+        },
+
         innerComplexPropery:function(el) {
 
             var id = el.getAttribute("data-id");
@@ -188,6 +239,7 @@
         }
     }
 
+    //dom manipulation utilities
     var dom = {
         //get an element by ID
         gid: function(id) {
@@ -225,9 +277,20 @@
                     parent.appendChild(d.firstChild);
                 }
             }
+        },
+        //get the previous sibling of a node that is a specific node name
+        prevSib:function(node, nodeName) {
+
+            var found = node.previousSibling;
+            while (found != null && found.nodeName.toLowerCase() != nodeName) {
+                found = found.previousSibling;
+            }
+            return found;
+
         }
     }
 
+    //ajax to get the template html
     function getTemplate(callback) {
 
         var xhr = new XMLHttpRequest();
@@ -253,7 +316,7 @@
     }
 
 
-
+    //ajax to get a json object form the server
     function getJson(url, callback) {
 
         var xhr = new XMLHttpRequest();
