a7d7aca3812dc6967e1a129b3bd5e5741ee49e88
matthewcv <accounts@matthewcv.com>
2015-02-05 09:21:36 -0600

    Revert "react"
    This reverts commit 35d349536436cf4f4524af1c6616ad8a0034cd05.

--- WebApiTestClient.Web/Content/WebApiTestClient/views.jsx
+++ WebApiTestClient.Web/Content/WebApiTestClient/views.jsx
@@ -1,20 +1,9 @@
 var WatcPanel = React.createClass({
 
-
   render:function(){
-      console.log('in component');
-      console.dir(this);
     return(
       <div id='watc-panel'>
 
-
-          <div class="watc-container">
-
-
-              <div><span id="watc-method">{this.props.Method}</span> <span id="watc-route">{this.props.Route}</span></div>
-
-
-          </div>
       </div>
     )
   }
